f549b5dc0e61df1cd3838498b8c307914766c1c3
seamus Herrod <sharkbait@seamuss-MacBook-Air.local>
2024-02-07 01:34:16 -0600

    bfs finished

--- ai_ps3/src/main.rs
+++ ai_ps3/src/main.rs
@@ -11,13 +11,12 @@ fn main() {
                 [0,0,0,0,0,1,0,0],
               ]
     };
-    // dbg!(grid);
+    // solving the example grid
     let source: Node = Node {x: 3, y: 2, cost: 0};
-    let dest: Node = Node {x: 3, y: 6, cost: 0};
-    println!("source: {:?}", source);
-    println!("dest: {:?}", dest);
-    println!("grid_dest: {:?}", grid.grid[dest.x as usize][dest.y as usize]);
+    //let dest: Node = Node {x: 3, y: 6, cost: 0};
+    let dest: Node = Node {x: 2, y: 2, cost: 0};
     let solution = breadth_first_search(&grid, source, dest);
+    
     dbg!(solution);
 
 }
@@ -38,6 +37,8 @@ impl  ExampleGrid {
 struct Solution {
     node: Node,
     cost: i32,
+    closed: usize,
+    fringe: i32,
 
 }
 
@@ -57,9 +58,12 @@ fn breadth_first_search(grid: &ExampleGrid, source: Node, dest: Node) -> Option<
     queue.push_back(source.clone());
     while !queue.is_empty() {
         let cur_node = queue.pop_front().unwrap();
-        dbg!(&cur_node);
+        //dbg!(&cur_node);
+        let fringe = bfs_fringe_set(queue.clone(), &visited, &grid); // calculate fringe nodes
         if cur_node.x == dest.x && cur_node.y == dest.y{
-            return Some(Solution {cost: cur_node.cost, node: cur_node.clone()});
+            return Some(Solution {cost: cur_node.cost, node: cur_node.clone(), 
+                                  closed: bfs_closed_set(&visited),
+                                  fringe: fringe});
         }
         for &(i, j) in &moves {
             if cur_node.x + i >= 0 && cur_node.y + j >= 0
@@ -77,4 +81,38 @@ fn breadth_first_search(grid: &ExampleGrid, source: Node, dest: Node) -> Option<
     None
 }
 
+fn bfs_closed_set(visited: &Vec<Vec<bool>>) ->  usize {
+    let mut closed: usize = 0;
+    for i in 0..visited.len() {
+        for j in 0..visited[0].len() {
+            if visited[i][j] {
+                closed += 1;
+            }
+        }
+    }
+    closed
+}
 
+fn bfs_fringe_set(queue: VecDeque<Node>, visited: &Vec<Vec<bool>>, grid: &ExampleGrid) -> i32 {
+    let mut fringe: i32 = 0;
+    for node in queue {
+        fringe += num_reachable_nodes(grid, &node, visited);
+    }
+    fringe
+}
+
+fn num_reachable_nodes(grid: &ExampleGrid, node: &Node, visited: &Vec<Vec<bool>>) -> i32 {
+    let moves = [(1,0), (0,-1), (-1,0), (0,1)];
+    let mut reachable = 0;
+    for &(i,j) in &moves {
+        if node.x + i >= 0 && node.y + j >= 0
+        && node.x + i < grid.grid.len() as isize 
+        && node.y + j < grid.grid[0].len() as isize {
+            let new_node = Node {x: node.x + i, y: node.y + j, cost: 0};
+            if grid.valid_move(new_node.clone()) && !visited[new_node.x as usize][new_node.y as usize] {
+                reachable += 1;
+            }
+        }
+    }
+    reachable
+}
